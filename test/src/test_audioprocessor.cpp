33eab64ad58fd5dadaeca85f94419913722f678c
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-11-12 09:31:49 +0330

    add google test support

---
+++ test/src/test_audioprocessor.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <PluginProcessor.h>
+
+namespace sample_audio_plugin_test
+{
+
+TEST(SampleAudioProcessorPlugin, Foo) {
+    AudioPluginAudioProcessor processor{};
+    ASSERT_FALSE(true);
+}
+    
+} // namespace sample_audio_plugin_test
